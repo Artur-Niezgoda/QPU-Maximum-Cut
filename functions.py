7af67b06acd6a676e8e36a119e2e0656911ab583
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2025-05-15 19:03:04 -0400

    Map classical inputs to a quantum problem.

---
+++ functions.py
@@ -0,0 +1,20 @@
+from qiskit.quantum_info import SparsePauliOp
+import rustworkx as rx
+
+
+def build_max_cut_paulis(graph: rx.PyGraph) -> list[tuple[str, float]]:
+    """Convert the graph to Pauli list.
+
+    This function does the inverse of `build_max_cut_graph`
+    """
+    pauli_list = []
+    for edge in list(graph.edge_list()):
+        paulis = ["I"] * len(graph)
+        paulis[edge[0]], paulis[edge[1]] = "Z", "Z"
+
+        weight = graph.get_edge_data(edge[0], edge[1])
+
+        pauli_list.append(("".join(paulis)[::-1], weight))
+
+    return pauli_list
+
--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import rustworkx as rx
 from rustworkx.visualization import mpl_draw as draw_graph
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import build_max_cut_paulis
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.circuit.library import QAOAAnsatz
 
 
 # Graph initialization
 
 n = 5 # number of nodes
-line_len = 1.0
+weight = 1.0
 
 graph = rx.PyGraph()
 graph.add_nodes_from(np.arange(0, n, 1))
-edge_list = [(0, 1, line_len), (0, 2, line_len), (0, 4, line_len), (1, 2, line_len),
-              (2, 3, line_len), (3, 4, line_len)]
+edge_list = [(0, 1, weight), (0, 2, weight), (0, 4, weight), (1, 2, weight),
+              (2, 3, weight), (3, 4, weight)]
 graph.add_edges_from(edge_list)
 draw_graph(graph, node_size=600, with_labels=True)
-# plt.show()
+#plt.clf()
+#plt.show()
 
 # STEP 1: Map classical inputs to a quantum problem
+
+max_cut_paulis = build_max_cut_paulis(graph)
+cost_hamiltonian = SparsePauliOp.from_list(max_cut_paulis) # create cost Hamiltonian
+print("Cost Function Hamiltonian:", cost_hamiltonian)
+
+circuit = QAOAAnsatz(cost_operator=cost_hamiltonian, reps=2)
+circuit.measure_all()
+
+#circuit.draw('mpl')
+
 
